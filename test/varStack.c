0bd2188a27d985cc6d980fcbc7d18cf9d53bd113
Oskar Lindholm <oskar_lindholm_ritz@hotmail.com>
2016-12-11 16:12:38 +0100

    stacktest

--- test/varStack.c
+++ test/varStack.c
@@ -13,6 +13,10 @@ void hej()
     int i = 0;
     
     printf("Here ends the stack:%p\n\n", environ); 
+    if(environ > caller )
+        printf("%s\n","stack grows down" );
+    
+    else printf("%s\n", "the stack grows upp");
     //printf("int%p",ha);
 }
 
