e1392ba7e54b1a1c07ace76747fbcf71b27fa162
Oskar Lindholm <oskar_lindholm_ritz@hotmail.com>
2016-12-13 12:13:50 +0100

    naiv implementation of h_alloc_struct added

--- heap/main.c
+++ heap/main.c
@@ -6,6 +6,10 @@ int main(int argc, char *argv[])
     int *haj = &hello;
     char *hej = "iiiicc";
     printf("%i\n",format_size(hej));
+    printf("%i\n", &hello);
+    
+    int her = haj+1;
+    printf("%i\n", her);
     //read_string("hej");        
     return 0;
 
